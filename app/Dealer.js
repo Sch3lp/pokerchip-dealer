f1ee7967ea2086961f3ba6ca90318ba6f19e4e22
Tim Schraepen <sch3lp@gmail.com>
2016-07-17 14:22:08 +0200

    number of different chipvalues required

--- app/Dealer.js
+++ app/Dealer.js
@@ -5,7 +5,21 @@ module.exports = (() => {
 
 class Dealer {
 
+	constructor(totalAmountOfChips, amountOfPossibleValues, amountOfPlayers, buyIn) {
+		this.totalAmountOfChips = totalAmountOfChips;
+		this.amountOfPossibleValues = amountOfPossibleValues;
+		this.amountOfPlayers = amountOfPlayers;
+		this.buyIn = buyIn;
+	}
+
 	deal() {
+		if (!this.totalAmountOfChips || this.totalAmountOfChips <= 0) {
+			return 'I require a number of chips before dealing.';
+ 		}
+ 		if (!this.amountOfPossibleValues || this.amountOfPossibleValues <= 0) {
+			return 'I require a number of possible chip values before dealing.';
+ 		}
+
 		return '';
 	}
 }
--- tests/DealerTest.js
+++ tests/DealerTest.js
@@ -6,6 +6,10 @@ var myDealer = new Dealer();
 describe('Dealer', () => {
 	it('requires a number of chips', () => {
 		let dealer = new Dealer();
-		expect(dealer.deal(6)).to.equal('Number of chips required before dealing!');
+		expect(dealer.deal(6)).to.equal('I require a number of chips before dealing.');
+	});
+	it('requires an amount of possible values of chips', () => {
+		let dealer = new Dealer(10,0,0,0);
+		expect(dealer.deal(6)).to.equal('I require a number of possible chip values before dealing.');
 	});
 });
