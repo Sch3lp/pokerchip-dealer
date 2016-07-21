90f6e3a8834ae1beeb29fab45f2d467fa3364767
Tim Schraepen <sch3lp@gmail.com>
2016-07-21 16:34:00 +0200

    using combine after switch sb and bb is way smarter

--- app/ColorPicker.js
+++ app/ColorPicker.js
@@ -8,45 +8,22 @@ class ColorPicker {
 		if (!denominations || denominations.length == 0 || !availableColors || availableColors.length ==0) {
 			return [];
 		}
-		let [sb, bb, ...rest] = denominations;
-		if (availableColors.length == 1) {
-			return [{
-				color: availableColors[0][0],
-				amount: availableColors[0][1],
-				denomination: bb
-			}];
-		}
-
-		let sortedColors = Array.from(availableColors.sort(byMostAvailableColor));
-		let mostAvailableColor = sortedColors.shift();
-		let nextToMostAvailableColor = sortedColors.shift();
+		let denoms = Array.from(denominations);
+		let sb = denoms.shift();
+		let bb = denoms.shift();
+		denoms.unshift(bb, sb);
+		let sortedColors = Array.from(availableColors.sort(byMostAvailableColorDesc));
 		
-		let result = [
-			{
-				color: nextToMostAvailableColor[0],
-				amount: nextToMostAvailableColor[1],
-				denomination: sb
-			},
-			{
-				color: mostAvailableColor[0],
-				amount: mostAvailableColor[1],
-				denomination: bb
-			}];
-
-		if (sortedColors && sortedColors.length > 0) {
-			// safe assumption that sortedColors and rest are equal in size
-			// since denoms were determined based on pokerset
-			// and colors come from pokerset as well
-			// on the other hand, colors is most constraining anyways
-			// TODO: refactor this later
-			result = result.concat(combine(sortedColors, rest));
-		}
-		return result;
+		// safe assumption that sortedColors and rest are equal in size
+		// since denoms were determined based on pokerset
+		// and colors come from pokerset as well
+		// on the other hand, colors is most constraining anyways
+		// TODO: refactor this later
+		return combine(sortedColors, denoms).sort(byDenominationAsc);
 	}
-
 }
 
-function byMostAvailableColor([prevColor, prevAmount], [curColor, curAmount]) {
+function byMostAvailableColorDesc([prevColor, prevAmount], [curColor, curAmount]) {
 	return curAmount > prevAmount 
 		? curAmount == prevAmount ? 0 : 1
 		: -1;
@@ -60,6 +37,10 @@ function combine(colors, denoms) {
 			denomination: denoms[idx]
 		};
 	});
+}
+
+function byDenominationAsc(stack1, stack2) {
+	return stack1.denomination - stack2.denomination;
 }
 
 return ColorPicker;
