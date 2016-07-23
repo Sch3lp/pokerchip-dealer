3689de29f6277b623b24016ff4aabf2fac8d1ac4
Tim Schraepen <sch3lp@gmail.com>
2016-07-23 16:10:28 +0200

    more refactoring

--- tests/KnapsackSolverTest.js
+++ tests/KnapsackSolverTest.js
@@ -2,23 +2,22 @@ let expect = require('chai').expect;
 let KnapsackSolver = require('../app/knapsacksolver');
 
 describe('KnapsackSolver', function() {
-	let _1chips = [ {color:'white',amount:100,denomination:.05} ];
+	let _1chips = [{color:'white',amount:100,denomination:.05}];
 	let _2chips = _1chips.concat([{color:'red',amount:150,denomination:.1}]);
 	let _3chips = _2chips.concat([{color:'blue',amount:100,denomination:.25}]);
 	let _4chips = _3chips.concat([{color:'green',amount:100,denomination:.5}]);
 	let _5chips = _4chips.concat([{color:'black',amount:50,denomination:1}]);
+	
 	describe('applyValues', function() {
 		it('with 1 item => value is 1', function() {
-			let assignedChips = _1chips;
-			let items = KnapsackSolver.applyValues(assignedChips);
+			let items = KnapsackSolver.applyValues(_1chips);
 			let itemValues = toValueColorPairs(items);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:1}
 			]);
 		});
 		it('with 2 items => values are equal', function() {
-			let assignedChips = _2chips;
-			let items = KnapsackSolver.applyValues(assignedChips);
+			let items = KnapsackSolver.applyValues(_2chips);
 			let itemValues = toValueColorPairs(items);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:1},
@@ -26,8 +25,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('with 3 items => second item has highest value, last item lowest', function() {
-			let assignedChips = _3chips;
-			let items = KnapsackSolver.applyValues(assignedChips);
+			let items = KnapsackSolver.applyValues(_3chips);
 			let itemValues = toValueColorPairs(items);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:2},
@@ -36,8 +34,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('with 4 items => second item has highest value, 3rd item second highest, rest descend in values along position in item list', function() {
-			let assignedChips = _4chips;
-			let items = KnapsackSolver.applyValues(assignedChips);
+			let items = KnapsackSolver.applyValues(_4chips);
 			let itemValues = toValueColorPairs(items);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:2},
@@ -47,8 +44,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('with 5 items => same as with 4', function() {
-			let assignedChips = _5chips;
-			let items = KnapsackSolver.applyValues(assignedChips);
+			let items = KnapsackSolver.applyValues(_5chips);
 			let itemValues = toValueColorPairs(items);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:3},
@@ -60,7 +56,7 @@ describe('KnapsackSolver', function() {
 		});
 	});
 
-	describe.skip('applyWeights', function() {
+	describe.only('applyWeights', function() {
 		it('1 player => weights == total amount of chips', function() {
 
 		});
@@ -69,4 +65,8 @@ describe('KnapsackSolver', function() {
 
 function toValueColorPairs(items){
 	return items.map(({value,wrapped:{color}}) => {return {color,value};});
+}
+
+function toWeightColorPairs(items){
+	return items.map(({weight,wrapped:{color}}) => {return {color,weight};});
 }
