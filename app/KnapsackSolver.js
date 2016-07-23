243c3efafa810bb03ca097b8aa0b0e9154057542
Tim Schraepen <sch3lp@gmail.com>
2016-07-23 17:17:56 +0200

    methods are now 'private' in that you have to explicitly import them. can't think of alternative right away

--- app/KnapsackSolver.js
+++ app/KnapsackSolver.js
@@ -8,47 +8,56 @@ module.exports = (() => {
  */
 class KnapsackSolver {
 
-	static applyValues(assignedChips) {
-		if (assignedChips.length == 1) return [1];
-		if (assignedChips.length == 2) return [1,1];
-		if (assignedChips.length == 3) return [2, 3, 1];
-		let copy = Array.from(assignedChips);
-		let sb = copy.shift();
-		let bb = copy.shift();
-		let bbplus1 = copy.shift();
-		copy.unshift(bb,bbplus1,sb);
-		return copy
-		.reverse()
-		.map((el, idx) => {
-			return {
-				value: idx+1,
-				denomination: el.denomination
-			};
-		})//retain denom so I can retain idx position by sorting (fragile I know)
-		.sort(byDenominationAsc)
-		.map(({value}) => value);//just retain value
-	}
+	// static convertToItems(assignedChips, players) {
+	// 	let values = applyValues(assignedChips);
+	// 	let weights = applyWeights(assignedChips, players);
+	// 	return assignedChips.map((chip, idx) => {
+	// 		return {
+	// 			value: values[idx],
+	// 			weight: weights[idx],
+	// 			chip: chip
+	// 		};
+	// 	});
+	// }
 
-	static applyWeights(items, players) {
-		return items.map((el) => floor(el.amount / players, 2));
-	}
 }
 
-function byDenominationAsc(values1, values2) {
-	return values1.denomination - values2.denomination;
+
+function applyValues(assignedChips) {
+	if (assignedChips.length == 1) return [1];
+	if (assignedChips.length == 2) return [1,1];
+	if (assignedChips.length == 3) return [2, 3, 1];
+	let copy = Array.from(assignedChips);
+	let sb = copy.shift();
+	let bb = copy.shift();
+	let bbplus1 = copy.shift();
+	copy.unshift(bb,bbplus1,sb);
+	return copy
+	.reverse()
+	.map((el, idx) => {
+		return {
+			value: idx+1,
+			denomination: el.denomination
+		};
+	})//retain denom so I can retain idx position by sorting (fragile I know)
+	.sort((values1, values2) => values1.denomination - values2.denomination)
+	.map(({value}) => value);//just retain value
 }
 
-/*
- * Thanks MDN ♥︎
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#Example:_Decimal_rounding
- */
-function floor(number, precision) {
-    var factor = Math.pow(10, precision);
-    var tempNumber = number * factor;
-    var roundedTempNumber = Math.floor(tempNumber);
-    return roundedTempNumber / factor;
-};
+function applyWeights(items, players) {
+	/*
+	 * Thanks MDN ♥︎
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#Example:_Decimal_rounding
+	 */
+	function floor(number, precision) {
+	    var factor = Math.pow(10, precision);
+	    var tempNumber = number * factor;
+	    var roundedTempNumber = Math.floor(tempNumber);
+	    return roundedTempNumber / factor;
+	};
+	return items.map((el) => floor(el.amount / players, 2));
+}
 
-return KnapsackSolver;
+return {KnapsackSolver, applyValues, applyWeights};
 
 })();
--- tests/KnapsackSolverTest.js
+++ tests/KnapsackSolverTest.js
@@ -1,7 +1,7 @@
 let expect = require('chai').expect;
-let KnapsackSolver = require('../app/knapsacksolver');
+let {KnapsackSolver, applyValues, applyWeights} = require('../app/knapsacksolver');
 
-describe('KnapsackSolver', function() {
+describe.only('KnapsackSolver', function() {
 	let _1chips = [{color:'white',amount:100,denomination:.05}];
 	let _2chips = _1chips.concat([{color:'red',amount:150,denomination:.1}]);
 	let _3chips = _2chips.concat([{color:'blue',amount:100,denomination:.25}]);
@@ -10,14 +10,14 @@ describe('KnapsackSolver', function() {
 	
 	describe('applyValues', function() {
 		it('with 1 item => value is 1', function() {
-			let items = KnapsackSolver.applyValues(_1chips);
+			let items = applyValues(_1chips);
 			let itemValues = toValueColorPairs(items,_1chips);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:1}
 			]);
 		});
 		it('with 2 items => values are equal', function() {
-			let items = KnapsackSolver.applyValues(_2chips);
+			let items = applyValues(_2chips);
 			let itemValues = toValueColorPairs(items,_2chips);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:1},
@@ -25,7 +25,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('with 3 items => second item has highest value, last item lowest', function() {
-			let items = KnapsackSolver.applyValues(_3chips);
+			let items = applyValues(_3chips);
 			let itemValues = toValueColorPairs(items,_3chips);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:2},
@@ -34,7 +34,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('with 4 items => second item has highest value, 3rd item second highest, rest descend in values along position in item list', function() {
-			let items = KnapsackSolver.applyValues(_4chips);
+			let items = applyValues(_4chips);
 			let itemValues = toValueColorPairs(items,_4chips);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:2},
@@ -44,7 +44,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('with 5 items => same as with 4', function() {
-			let items = KnapsackSolver.applyValues(_5chips);
+			let items = applyValues(_5chips);
 			let itemValues = toValueColorPairs(items,_5chips);
 			expect(itemValues).to.deep.equal([
 				{color:'white',	value:3},
@@ -56,16 +56,16 @@ describe('KnapsackSolver', function() {
 		});
 	});
 
-	describe.only('applyWeights', function() {
+	describe('applyWeights', function() {
 		it('2 player, 1 item => weights == total amount of chips', function() {
-			let weights = KnapsackSolver.applyWeights(_1chips,2);
+			let weights = applyWeights(_1chips,2);
 			let itemWeights = toWeightColorPairs(weights,_1chips);
 			expect(itemWeights).to.deep.equal([
 				{color:'white', 	weight: 50 }
 			]);
 		});
 		it('1 player => weights == total amount of chips', function() {
-			let weights = KnapsackSolver.applyWeights(_5chips,1);
+			let weights = applyWeights(_5chips,1);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
 			expect(itemWeights).to.deep.equal([
 				{color:'white', 	weight: 100 },
@@ -76,7 +76,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('3 players => weights == total amount of chips divided by 3', function() {
-			let weights = KnapsackSolver.applyWeights(_5chips,3);
+			let weights = applyWeights(_5chips,3);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
 			expect(itemWeights).to.deep.equal([
 				{color:'white', 	weight:  33.33  },
@@ -87,7 +87,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('6 players => weights == total amount of chips divided by 6', function() {
-			let weights = KnapsackSolver.applyWeights(_5chips,6);
+			let weights = applyWeights(_5chips,6);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
 			expect(itemWeights).to.deep.equal([
 				{color:'white', 	weight: 16.66   },
@@ -98,7 +98,7 @@ describe('KnapsackSolver', function() {
 			]);
 		});
 		it('10 players => weights == total amount of chips divided by 10', function() {
-			let weights = KnapsackSolver.applyWeights(_5chips,10);
+			let weights = applyWeights(_5chips,10);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
 			expect(itemWeights).to.deep.equal([
 				{color:'white', 	weight: 10  },
