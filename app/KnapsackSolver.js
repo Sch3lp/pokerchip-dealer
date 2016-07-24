224fbe8632316a9ab5bad70739c5e6fad13529d9
Tim Schraepen <sch3lp@gmail.com>
2016-07-24 22:45:03 +0200

    Nope, was right all along. Is just weird to constantly have to map that stuff in my brain.
    This reverts commit 7d7b70855f26e221f18cd179d4415d5cd9602abe.

--- app/KnapsackSolver.js
+++ app/KnapsackSolver.js
@@ -8,19 +8,15 @@ module.exports = (() => {
  */
 class KnapsackSolver {
 
-	constructor(assignedChips, players) {
+	constructor(assignedChips, players){
 		this.items = convertToItems(assignedChips, players);
 	}
 
-	solve(knapsackSize) {
-		//size == buyin
-
-	}
 }
 
 function convertToItems(assignedChips, players) {
-	let weights = applyWeights(assignedChips);
-	let values = applyValues(assignedChips, players);
+	let values = applyValues(assignedChips);
+	let weights = applyWeights(assignedChips, players);
 	return assignedChips.map((chip, idx) => {
 		return {
 			value: values[idx],
@@ -30,7 +26,7 @@ function convertToItems(assignedChips, players) {
 	});
 }
 
-function applyWeights(assignedChips) {
+function applyValues(assignedChips) {
 	if (assignedChips.length == 1) return [1];
 	if (assignedChips.length == 2) return [1,1];
 	if (assignedChips.length == 3) return [2, 3, 1];
@@ -40,18 +36,18 @@ function applyWeights(assignedChips) {
 	let bbplus1 = copy.shift();
 	copy.unshift(bb,bbplus1,sb);
 	return copy
-		.reverse()
-		.map((el, idx) => {
-			return {
-				weight: idx+1,
-				denomination: el.denomination
-			};
-		})//retain denom so I can retain idx position by sorting (fragile I know)
-		.sort((weights1, weights2) => weights1.denomination - weights2.denomination)
-		.map(({weight}) => weight);//just retain weight
+	.reverse()
+	.map((el, idx) => {
+		return {
+			value: idx+1,
+			denomination: el.denomination
+		};
+	})//retain denom so I can retain idx position by sorting (fragile I know)
+	.sort((values1, values2) => values1.denomination - values2.denomination)
+	.map(({value}) => value);//just retain value
 }
 
-function applyValues(items, players) {
+function applyWeights(items, players) {
 	/*
 	 * Thanks MDN ♥︎
 	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#Example:_Decimal_rounding
--- tests/KnapsackSolverTest.js
+++ tests/KnapsackSolverTest.js
@@ -1,5 +1,5 @@
 let expect = require('chai').expect;
-let {KnapsackSolver, applyWeights, applyValues} = require('../app/knapsacksolver');
+let {KnapsackSolver, applyValues, applyWeights} = require('../app/knapsacksolver');
 
 describe.only('KnapsackSolver', function() {
 	let _1chips =                [{color:'white',amount:100,denomination:.05}];
@@ -8,104 +8,104 @@ describe.only('KnapsackSolver', function() {
 	let _4chips = _3chips.concat([{color:'green',amount:75,denomination:.5}]);
 	let _5chips = _4chips.concat([{color:'black',amount:50,denomination:1}]);
 	
-	describe('applyWeights', function() {
-		it('with 1 item => weight is 1', function() {
-			let items = applyWeights(_1chips);
-			let itemWeights = toWeightColorPairs(items,_1chips);
-			expect(itemWeights).to.deep.equal([
-				{color:'white',	weight:1}
+	describe('applyValues', function() {
+		it('with 1 item => value is 1', function() {
+			let items = applyValues(_1chips);
+			let itemValues = toValueColorPairs(items,_1chips);
+			expect(itemValues).to.deep.equal([
+				{color:'white',	value:1}
 			]);
 		});
-		it('with 2 items => weights are equal', function() {
-			let items = applyWeights(_2chips);
-			let itemWeights = toWeightColorPairs(items,_2chips);
-			expect(itemWeights).to.deep.equal([
-				{color:'white',	weight:1},
-				{color:'red',	weight:1}
+		it('with 2 items => values are equal', function() {
+			let items = applyValues(_2chips);
+			let itemValues = toValueColorPairs(items,_2chips);
+			expect(itemValues).to.deep.equal([
+				{color:'white',	value:1},
+				{color:'red',	value:1}
 			]);
 		});
-		it('with 3 items => second item has highest weight, last item lowest', function() {
-			let items = applyWeights(_3chips);
-			let itemWeights = toWeightColorPairs(items,_3chips);
-			expect(itemWeights).to.deep.equal([
-				{color:'white',	weight:2},
-				{color:'red',	weight:3},
-				{color:'blue',	weight:1}
+		it('with 3 items => second item has highest value, last item lowest', function() {
+			let items = applyValues(_3chips);
+			let itemValues = toValueColorPairs(items,_3chips);
+			expect(itemValues).to.deep.equal([
+				{color:'white',	value:2},
+				{color:'red',	value:3},
+				{color:'blue',	value:1}
 			]);
 		});
-		it('with 4 items => second item has highest weight, 3rd item second highest, rest descend in values along position in item list', function() {
-			let items = applyWeights(_4chips);
-			let itemWeights = toWeightColorPairs(items,_4chips);
-			expect(itemWeights).to.deep.equal([
-				{color:'white',	weight:2},
-				{color:'red',	weight:4},
-				{color:'blue',	weight:3},
-				{color:'green',	weight:1}
+		it('with 4 items => second item has highest value, 3rd item second highest, rest descend in values along position in item list', function() {
+			let items = applyValues(_4chips);
+			let itemValues = toValueColorPairs(items,_4chips);
+			expect(itemValues).to.deep.equal([
+				{color:'white',	value:2},
+				{color:'red',	value:4},
+				{color:'blue',	value:3},
+				{color:'green',	value:1}
 			]);
 		});
 		it('with 5 items => same as with 4', function() {
-			let items = applyWeights(_5chips);
-			let itemWeights = toWeightColorPairs(items,_5chips);
-			expect(itemWeights).to.deep.equal([
-				{color:'white',	weight:3},
-				{color:'red',	weight:5},
-				{color:'blue',	weight:4},
-				{color:'green',	weight:2},
-				{color:'black',	weight:1}
+			let items = applyValues(_5chips);
+			let itemValues = toValueColorPairs(items,_5chips);
+			expect(itemValues).to.deep.equal([
+				{color:'white',	value:3},
+				{color:'red',	value:5},
+				{color:'blue',	value:4},
+				{color:'green',	value:2},
+				{color:'black',	value:1}
 			]);
 		});
 	});
 
-	describe('applyValues', function() {
-		it('2 player, 1 item => values == total amount of chips', function() {
-			let values = applyValues(_1chips,2);
-			let itemValues = toValueColorPairs(values,_1chips);
-			expect(itemValues).to.deep.equal([
-				{color:'white', 	value: 50 }
+	describe('applyWeights', function() {
+		it('2 player, 1 item => weights == total amount of chips', function() {
+			let weights = applyWeights(_1chips,2);
+			let itemWeights = toWeightColorPairs(weights,_1chips);
+			expect(itemWeights).to.deep.equal([
+				{color:'white', 	weight: 50 }
 			]);
 		});
-		it('1 player => values == total amount of chips', function() {
-			let values = applyValues(_5chips,1);
-			let itemValues = toValueColorPairs(values,_5chips);
-			expect(itemValues).to.deep.equal([
-				{color:'white', 	value: 100 },
-				{color:'red',		value: 150	},
-				{color:'blue',		value: 100	},
-				{color:'green',		value:  75	},
-				{color:'black',		value:  50	}
+		it('1 player => weights == total amount of chips', function() {
+			let weights = applyWeights(_5chips,1);
+			let itemWeights = toWeightColorPairs(weights,_5chips);
+			expect(itemWeights).to.deep.equal([
+				{color:'white', 	weight: 100 },
+				{color:'red',		weight: 150	},
+				{color:'blue',		weight: 100	},
+				{color:'green',		weight:  75	},
+				{color:'black',		weight:  50	}
 			]);
 		});
-		it('3 players => values == total amount of chips divided by 3', function() {
-			let values = applyValues(_5chips,3);
-			let itemValues = toValueColorPairs(values,_5chips);
-			expect(itemValues).to.deep.equal([
-				{color:'white', 	value:  33.33  },
-				{color:'red',		value:  50.00	},
-				{color:'blue',		value:  33.33	},
-				{color:'green',		value:  25.00	},
-				{color:'black',		value:  16.66	}
+		it('3 players => weights == total amount of chips divided by 3', function() {
+			let weights = applyWeights(_5chips,3);
+			let itemWeights = toWeightColorPairs(weights,_5chips);
+			expect(itemWeights).to.deep.equal([
+				{color:'white', 	weight:  33.33  },
+				{color:'red',		weight:  50.00	},
+				{color:'blue',		weight:  33.33	},
+				{color:'green',		weight:  25.00	},
+				{color:'black',		weight:  16.66	}
 			]);
 		});
-		it('6 players => values == total amount of chips divided by 6', function() {
-			let values = applyValues(_5chips,6);
-			let itemValues = toValueColorPairs(values,_5chips);
-			expect(itemValues).to.deep.equal([
-				{color:'white', 	value: 16.66   },
-				{color:'red',		value: 25	    },
-				{color:'blue',		value: 16.66	},
-				{color:'green',		value: 12.5	},
-				{color:'black',		value:  8.33	}
+		it('6 players => weights == total amount of chips divided by 6', function() {
+			let weights = applyWeights(_5chips,6);
+			let itemWeights = toWeightColorPairs(weights,_5chips);
+			expect(itemWeights).to.deep.equal([
+				{color:'white', 	weight: 16.66   },
+				{color:'red',		weight: 25	    },
+				{color:'blue',		weight: 16.66	},
+				{color:'green',		weight: 12.5	},
+				{color:'black',		weight:  8.33	}
 			]);
 		});
-		it('10 players => values == total amount of chips divided by 10', function() {
-			let values = applyValues(_5chips,10);
-			let itemValues = toValueColorPairs(values,_5chips);
-			expect(itemValues).to.deep.equal([
-				{color:'white', 	value: 10  },
-				{color:'red',		value: 15  },
-				{color:'blue',		value: 10	},
-				{color:'green',		value:  7.5},
-				{color:'black',		value:  5	}
+		it('10 players => weights == total amount of chips divided by 10', function() {
+			let weights = applyWeights(_5chips,10);
+			let itemWeights = toWeightColorPairs(weights,_5chips);
+			expect(itemWeights).to.deep.equal([
+				{color:'white', 	weight: 10  },
+				{color:'red',		weight: 15  },
+				{color:'blue',		weight: 10	},
+				{color:'green',		weight:  7.5},
+				{color:'black',		weight:  5	}
 			]);
 		});
 	});
@@ -114,20 +114,20 @@ describe.only('KnapsackSolver', function() {
 		it('converts assigned chips to valued and weighted items for use in knapsack', function() {
 			let items = new KnapsackSolver(_5chips, 6).items;
 			expect(items).to.deep.equal([
-		{value: 16.66,	weight:3,	chip: {color:'white',amount:100,denomination:.05}},
-		{value: 25, 	weight:5,	chip: {color:'red',amount:150,denomination:.1}},
-		{value: 16.66,	weight:4,	chip: {color:'blue',amount:100,denomination:.25}},
-		{value: 12.5,	weight:2,	chip: {color:'green',amount:75,denomination:.5}},
-		{value:  8.33,	weight:1,	chip: {color:'black',amount:50,denomination:1}}
+		{value:3,	weight: 16.66,	chip: {color:'white',amount:100,denomination:.05}},
+		{value:5,	weight: 25, 	chip: {color:'red',amount:150,denomination:.1}},
+		{value:4,	weight: 16.66,	chip: {color:'blue',amount:100,denomination:.25}},
+		{value:2,	weight: 12.5,	chip: {color:'green',amount:75,denomination:.5}},
+		{value:1,	weight:  8.33,	chip: {color:'black',amount:50,denomination:1}}
 			]);
 		});
 	});
 });
 
-function toWeightColorPairs(weights, chips){
-	return weights.map((weight, idx) => {return {color:chips[idx].color,weight};});
-}
-
 function toValueColorPairs(values, chips){
 	return values.map((value, idx) => {return {color:chips[idx].color,value};});
+}
+
+function toWeightColorPairs(weights, chips){
+	return weights.map((weight, idx) => {return {color:chips[idx].color,weight};});
 }
