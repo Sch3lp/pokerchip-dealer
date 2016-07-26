78a67e9c70de23367e4664add6813ddf9ae66f5e
Tim Schraepen <sch3lp@gmail.com>
2016-07-26 20:27:12 +0200

    attack first test

--- tests/KnapsackSolverTest.js
+++ tests/KnapsackSolverTest.js
@@ -18,6 +18,7 @@ describe('KnapsackSolver', function() {
 	];
 
 	describe('applyValues', function() {
+		
 		it('with 1 item => value is 1', function() {
 			let items = applyValues(_1chips);
 			let itemValues = toValueColorPairs(items,_1chips);
@@ -25,6 +26,7 @@ describe('KnapsackSolver', function() {
 				{color:'white',	value:1}
 			]);
 		});
+		
 		it('with 2 items => values are equal', function() {
 			let items = applyValues(_2chips);
 			let itemValues = toValueColorPairs(items,_2chips);
@@ -33,6 +35,7 @@ describe('KnapsackSolver', function() {
 				{color:'red',	value:1}
 			]);
 		});
+		
 		it('with 3 items => second item has highest value, last item lowest', function() {
 			let items = applyValues(_3chips);
 			let itemValues = toValueColorPairs(items,_3chips);
@@ -42,6 +45,7 @@ describe('KnapsackSolver', function() {
 				{color:'blue',	value:1}
 			]);
 		});
+		
 		it('with 4 items => second item has highest value, 3rd item second highest, rest descend in values along position in item list', function() {
 			let items = applyValues(_4chips);
 			let itemValues = toValueColorPairs(items,_4chips);
@@ -52,6 +56,7 @@ describe('KnapsackSolver', function() {
 				{color:'green',	value:1}
 			]);
 		});
+		
 		it('with 5 items => same as with 4', function() {
 			let items = applyValues(_5chips);
 			let itemValues = toValueColorPairs(items,_5chips);
@@ -66,6 +71,7 @@ describe('KnapsackSolver', function() {
 	});
 
 	describe('applyWeights', function() {
+		
 		it('2 player, 1 item => weights == total amount of chips', function() {
 			let weights = applyWeights(_1chips,2);
 			let itemWeights = toWeightColorPairs(weights,_1chips);
@@ -73,6 +79,7 @@ describe('KnapsackSolver', function() {
 				{color:'white', 	weight: 50 }
 			]);
 		});
+		
 		it('1 player => weights == total amount of chips', function() {
 			let weights = applyWeights(_5chips,1);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
@@ -84,6 +91,7 @@ describe('KnapsackSolver', function() {
 				{color:'black',		weight: 425	}
 			]);
 		});
+		
 		it('3 players => weights == total amount of chips divided by 3', function() {
 			let weights = applyWeights(_5chips,3);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
@@ -95,6 +103,7 @@ describe('KnapsackSolver', function() {
 				{color:'black',		weight:  458.34	}
 			]);
 		});
+		
 		it('6 players => weights == total amount of chips divided by 6', function() {
 			let weights = applyWeights(_5chips,6);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
@@ -106,6 +115,7 @@ describe('KnapsackSolver', function() {
 				{color:'black',		weight: 466.67	}
 			]);
 		});
+		
 		it('10 players => weights == total amount of chips divided by 10', function() {
 			let weights = applyWeights(_5chips,10);
 			let itemWeights = toWeightColorPairs(weights,_5chips);
@@ -120,6 +130,7 @@ describe('KnapsackSolver', function() {
 	});
 
 	describe('constructor', function() {
+		
 		it('converts assigned chips to valued and weighted items for use in knapsack', function() {
 			let items = new KnapsackSolver(_5chips, 6).items;
 			expect(items).to.deep.equal([
@@ -132,7 +143,8 @@ describe('KnapsackSolver', function() {
 		});
 	});
 
-	describe('correctBySubtraction', function() {
+	describe.only('correctBySubtraction', function() {
+		
 		it('exactly one highest denomination overGreedy, removes 1 chip of that denomination', function() {
 			let overGreedy = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:15,denomination: 0.05}},
@@ -152,6 +164,7 @@ describe('KnapsackSolver', function() {
 			let correctedStack = correctBySubtraction(overGreedy,10);
 			expect(correctedStack).to.deep.equal(expectedCorrected);
 		});
+		
 		it('exactly one lowest denomination overGreedy, removes 1 chip of that denomination', function() {
 			let overGreedy = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:16,denomination: 0.05}},
@@ -171,6 +184,7 @@ describe('KnapsackSolver', function() {
 			let correctedStack = correctBySubtraction(overGreedy,10);
 			expect(correctedStack).to.deep.equal(expectedCorrected);
 		});
+		
 		it('exactly two denominations overGreedy, one small blind, one heighest denomination, removes both chips', function() {
 			let overGreedy = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:16,denomination: 0.05}},
@@ -190,6 +204,7 @@ describe('KnapsackSolver', function() {
 			let correctedStack = correctBySubtraction(overGreedy,10);
 			expect(correctedStack).to.deep.equal(expectedCorrected);
 		});
+		
 		it('more than one denomination overGreedy, removes chips with lowest value first', function() {
 			let overGreedy = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:16,denomination: 0.05}},
@@ -209,25 +224,28 @@ describe('KnapsackSolver', function() {
 			let correctedStack = correctBySubtraction(overGreedy,10);
 			expect(correctedStack).to.deep.equal(expectedCorrected);
 		});
-		it.skip('exactly two lowest and heighest valued denominations overGreedy, removes both chips', function() {
+		
+		it('exactly two lowest and heighest valued denominations overGreedy, removes both chips', function() {
 			let overGreedy = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:15,denomination: 0.05}},
 		{value:5, weight: 450.00, chip: {color:'red-blue',		amount:16,denomination: 0.1}},
-		{value:4, weight: 458.34, chip: {color:'blue-white',	amount:7,denomination: 0.25}},
-		{value:2, weight: 462.50, chip: {color:'green-pink',	amount:8,denomination: 0.5}},
-		{value:1, weight: 466.67, chip: {color:'black-salmon',	amount:3,denomination: 1}}
+		{value:4, weight: 458.34, chip: {color:'blue-white',	amount:7, denomination: 0.25}},
+		{value:2, weight: 462.50, chip: {color:'green-pink',	amount:8, denomination: 0.5}},
+		{value:1, weight: 466.67, chip: {color:'black-salmon',	amount:3, denomination: 1}}
 			];
 			let expectedCorrected = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:15,denomination: 0.05}},
 		{value:5, weight: 450.00, chip: {color:'red-blue',		amount:15,denomination: 0.1}},
-		{value:4, weight: 458.34, chip: {color:'blue-white',	amount:7,denomination: 0.25}},
-		{value:2, weight: 462.50, chip: {color:'green-pink',	amount:8,denomination: 0.5}},
-		{value:1, weight: 466.67, chip: {color:'black-salmon',	amount:2,denomination: 1}}
+		{value:4, weight: 458.34, chip: {color:'blue-white',	amount:7, denomination: 0.25}},
+		{value:2, weight: 462.50, chip: {color:'green-pink',	amount:8, denomination: 0.5}},
+		{value:1, weight: 466.67, chip: {color:'black-salmon',	amount:2, denomination: 1}}
 			];
 			expect(getStackWorth(overGreedy.map(({v,w,chip})=>chip))).to.equal(11.1);
 			let correctedStack = correctBySubtraction(overGreedy,10);
+			expect(getStackWorth(correctedStack.map(({v,w,chip})=>chip))).to.equal(10);
 			expect(correctedStack).to.deep.equal(expectedCorrected);
 		});
+		
 		it.skip('exactly two lowest and second to heighest valued denominations overGreedy, removes both chips', function() {
 			let overGreedy = [
 		{value:3, weight: 458.34, chip: {color:'white-red',		amount:15,denomination: 0.05}},
@@ -250,6 +268,7 @@ describe('KnapsackSolver', function() {
 	});
 
 	describe('solve', function() {
+		
 		it('buyin 10, 6 players', function() {
 			let buyin = 10;
 			let solver = new KnapsackSolver(myChips, 6);
@@ -263,12 +282,14 @@ describe('KnapsackSolver', function() {
 			]);
 			expect(getStackWorth(stack)).to.equal(buyin);
 		});
+		
 		it.skip('total worth of available chips is lower than buyin', function() {
 			let buyin = 10;
 			let solver = new KnapsackSolver(_3chips, 6);
 			let stack = solver.solve(buyin);
 			
 		});
+		
 		it('Stackworth must be equal to the buyin', function() {
 			let buyin = 10;
 			let solver = new KnapsackSolver(_5chips, 6);
