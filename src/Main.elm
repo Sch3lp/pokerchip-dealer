cf668de84ece11e1bd6d919d9f4fda509525b019
Tim Schraepen <sch3lp@gmail.com>
2017-07-09 18:59:06 +0200

    printing out pokerset

--- src/Main.elm
+++ src/Main.elm
@@ -6,9 +6,25 @@ import Update exposing (..)
 import View exposing (..)
 
 
+myPokerSet : PokerSet
+myPokerSet =
+    [ ( "purple", 75 )
+    , ( "orange", 75 )
+    , ( "white-red", 100 )
+    , ( "red-blue", 100 )
+    , ( "blue-white", 50 )
+    , ( "green-pink", 50 )
+    , ( "black-salmon", 25 )
+    , ( "purple-pink", 25 )
+    ]
+
+
 initialModel : Model
 initialModel =
-    { text = "cunts" }
+    { pokerset = myPokerSet
+    , buyin = 10
+    , players = 6
+    }
 
 
 main : Program Never Model Msg
--- src/Model.elm
+++ src/Model.elm
@@ -2,4 +2,33 @@ module Model exposing (..)
 
 
 type alias Model =
-    { text : String }
+    { pokerset : PokerSet
+    , buyin : Buyin
+    , players : Players
+    }
+
+
+type alias Players =
+    Int
+
+
+type alias Buyin =
+    Int
+
+
+type alias PokerSet =
+    List ChipsInColor
+
+
+type alias ChipsInColor =
+    ( String, Int )
+
+
+pokersetToString : PokerSet -> List String
+pokersetToString pokerset =
+    List.map chipsInColorToString pokerset
+
+
+chipsInColorToString : ChipsInColor -> String
+chipsInColorToString ( color, amount ) =
+    (toString amount) ++ " " ++ color ++ " chips"
