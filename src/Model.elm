9640063bf6c0e48985d43a83a5680cbb52130d7d
Tim Schraepen <sch3lp@gmail.com>
2017-07-18 18:58:48 +0200

    working with denom base

--- src/Model.elm
+++ src/Model.elm
@@ -89,14 +89,29 @@ type alias Denomination =
 
 convertToDenomBase : Float -> Int
 convertToDenomBase value =
-    round (value * 100)
+    round (value * denomBase)
+
+
+denomBase : number
+denomBase =
+    100
+
+
+standardDenoms : List Denomination
+standardDenoms =
+    [ 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 25, 50, 100 ]
 
 
 standardDenomValues : List Value
 standardDenomValues =
-    [ 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000 ]
+    List.map toValue standardDenoms
+
+
+toValue : Denomination -> Value
+toValue denom =
+    convertToDenomBase denom
 
 
 toDenom : Value -> Denomination
 toDenom val =
-    toFloat val / 100
+    toFloat val / denomBase
