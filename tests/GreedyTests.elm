517bc8c558b327b67c6524421b47061d4eeaf5bc
Tim Schraepen <sch3lp@gmail.com>
2017-08-23 16:10:31 +0200

    moved tests on model from greedytests to modeltests

--- tests/GreedyTests.elm
+++ tests/GreedyTests.elm
@@ -33,36 +33,7 @@ noPurplePokerset =
 
 greedyUnitTests : List Test
 greedyUnitTests =
-    [ describe "limitAmount"
-        [ test "Find limited amount of chips per color for 6 players" <|
-            \() ->
-                Expect.equal
-                    (limitAmount 6 pokerbrosPokerset)
-                    [ { color = "purple", amount = 12 }
-                    , { color = "orange", amount = 12 }
-                    , { color = "white-red", amount = 16 }
-                    , { color = "red-blue", amount = 16 }
-                    , { color = "blue-white", amount = 8 }
-                    , { color = "green-pink", amount = 8 }
-                    , { color = "black-salmon", amount = 4 }
-                    , { color = "purple-pink", amount = 4 }
-                    ]
-        ]
-    , describe "sortedByAmountDesc"
-        [ test "returns pokerset sortedByAmount Desc" <|
-            \() ->
-                Expect.equal
-                    (sortedByAmountDesc noPurplePokerset)
-                    [ { color = "white-red", amount = 100 }
-                    , { color = "red-blue", amount = 100 }
-                    , { color = "orange", amount = 75 }
-                    , { color = "blue-white", amount = 50 }
-                    , { color = "green-pink", amount = 50 }
-                    , { color = "black-salmon", amount = 25 }
-                    , { color = "purple-pink", amount = 25 }
-                    ]
-        ]
-    , describe "assignPreferredDenominationValues"
+    [ describe "assignPreferredDenominationValues"
         [ test "Highest amount is assigned big blind value, third highest small blind, second is third, fourth is fourth, ..." <|
             \() ->
                 Expect.equal
--- tests/ModelTests.elm
+++ tests/ModelTests.elm
@@ -16,10 +16,57 @@ modelUnitTests =
     [ convertToDenomBaseTests ]
 
 
+pokerbrosPokerset : PokerSet
+pokerbrosPokerset =
+    [ { color = "purple", amount = 75 }
+    , { color = "orange", amount = 75 }
+    , { color = "white-red", amount = 100 }
+    , { color = "red-blue", amount = 100 }
+    , { color = "blue-white", amount = 50 }
+    , { color = "green-pink", amount = 50 }
+    , { color = "black-salmon", amount = 25 }
+    , { color = "purple-pink", amount = 25 }
+    ]
+
+
+noPurplePokerset : PokerSet
+noPurplePokerset =
+    List.drop 1 pokerbrosPokerset
+
+
 convertToDenomBaseTests : Test
 convertToDenomBaseTests =
     describe "convertToDenomBase"
         [ test "should round" <|
             \() ->
                 Expect.equal (convertToDenomBase 19.4) 1940
+        , describe "limitAmount"
+            [ test "Find limited amount of chips per color for 6 players" <|
+                \() ->
+                    Expect.equal
+                        (limitAmount 6 pokerbrosPokerset)
+                        [ { color = "purple", amount = 12 }
+                        , { color = "orange", amount = 12 }
+                        , { color = "white-red", amount = 16 }
+                        , { color = "red-blue", amount = 16 }
+                        , { color = "blue-white", amount = 8 }
+                        , { color = "green-pink", amount = 8 }
+                        , { color = "black-salmon", amount = 4 }
+                        , { color = "purple-pink", amount = 4 }
+                        ]
+            ]
+        , describe "sortedByAmountDesc"
+            [ test "returns pokerset sortedByAmount Desc" <|
+                \() ->
+                    Expect.equal
+                        (sortedByAmountDesc noPurplePokerset)
+                        [ { color = "white-red", amount = 100 }
+                        , { color = "red-blue", amount = 100 }
+                        , { color = "orange", amount = 75 }
+                        , { color = "blue-white", amount = 50 }
+                        , { color = "green-pink", amount = 50 }
+                        , { color = "black-salmon", amount = 25 }
+                        , { color = "purple-pink", amount = 25 }
+                        ]
+            ]
         ]
