a8d6d3e7dcb57e9d0ece85fdd518b31f93477f96
Tim Schraepen <sch3lp@gmail.com>
2017-08-23 14:55:13 +0200

    updated todo

--- tests/DynamicProgrammingTests.elm
+++ tests/DynamicProgrammingTests.elm
@@ -6,8 +6,8 @@ import Model exposing (..)
 import DP exposing (..)
 
 
--- generate all possible combinations (not necessarily solutions)
--- by just making stacks by adding 1 chip every time (end when all chips, limited by players, have been used)
+-- generate all possible combinations (not necessarily solutions) [DONE]
+-- limited by players : get rid of combinations that have an amount which is non divisable by amount of players
 -- only keep the real solutions (stackworth == buyin)
 -- run an algorithm on those to retain the ideal solution:
 -- 0.1 chips maxed, then .25, then .05, and that ideally have at least one from the first 5 denoms in the stack
